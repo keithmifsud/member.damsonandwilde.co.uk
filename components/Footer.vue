176e2e4b49b566781c7947d78ef15d13cf6edaee
Keith Mifsud <mifsud.k@gmail.com>
2022-06-04 16:29:01 +0545

    :construction:

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="">
     <div class="flex flex-col">
-      <div class="flex flex-col underline underline-offset-1 text-sm"><NuxtLink to="terms">Terms & conditions</NuxtLink></div>
+      <div class="flex flex-col underline underline-offset-1 text-sm">
+        <NuxtLink to="terms">Terms & conditions</NuxtLink>
+      </div>
       <div class="flex flex-col">
         <div class="w-full">test</div>
       </div>
