72ccc59e5259df1fef469ce2251ea5b9d4339ad9
Keith Mifsud <mifsud.k@gmail.com>
2022-06-09 17:19:23 +0545

    :lipstick: Styled the footer.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="">
-    <div class="flex flex-col">
-      <div class="flex flex-col underline underline-offset-1 text-sm">
+    <div class="flex flex-col p-4">
+      <div class="flex flex-col underline underline-offset-1 text-sm text-center">
         <NuxtLink to="terms">Terms & conditions</NuxtLink>
       </div>
-      <div class="flex flex-col">
-        <div class="w-full">test</div>
+      <div class="flex flex-col text-center">
+        <div class="w-full text-xs mt-4">
+          Built with 🧡 by <span class="font-bold">bold</span>things.
+        </div>
       </div>
     </div>
   </div>
