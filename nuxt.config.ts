001a150adfcde35f6c19a5a35277b6ce5387d337
Keith Mifsud <mifsud.k@gmail.com>
2022-06-09 19:31:57 +0545

    :alien: :wrench: :alembic: Experimenting with missing MrYum API schema.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,17 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      GQL_HOST: 'https://clubcms.boldthin.gs/graphql',
+      'graphql-client': {
+        clients: {
+          default: 'https://clubcms.boldthin.gs/graphql',
+          mryum: {
+            schema: './schemas/mryum.graphql',
+            host: 'https://eu1-stable-api.mryum.com/graphql',//process.env.GQL_MRYUM_HOST,
+            token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzNTY5YmFhZi0wNzk1LTRjMGUtYWRhMy0wZjVjYzA1YTBmNjYiLCJzdWIiOiIxMDM5ODRjZi0wZmM2LTQwMTMtYWMxMC03MmViMDdkZDM0YjYiLCJpYXQiOjE2NDY4NjMyMzEsImlzcyI6Imh0dHBzOi8vZXUxLXByb2R1Y3Rpb24tc3RhYmxlLWFwaS5tcnl1bS5jb20iLCJhdWQiOiJodHRwczovL2V1MS1wcm9kdWN0aW9uLXN0YWJsZS1hcGkubXJ5dW0uY29tIiwiZXhwIjoxNjc4Mzk5MjMxfQ.MDTXIszIVWhmbPmplmoRQ6Mv8gMBxk34KRt3vKFfZl0'
+            //process.env.GQL_MRYUM_TOKEN,
+          }
+        },
+      },
       //http://localhost:1337/graphql', // overwritten by process.env.GQL_HOST,
       LOGIN_URL: process.env.LOGIN_URL || 'https://mryum.com',
       FALLBACK_VENUE_ID: process.env.FALLBACK_VENUE_ID || 'cpc-testing',
